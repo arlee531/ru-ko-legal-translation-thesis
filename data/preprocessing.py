cebe49e03388f9e830c3b02245042a9a338b83c2
arlee531 <ratm531@gmail.com>
2025-09-11 07:47:47 +0900

    Update preprocessing.py

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -1,1 +1,7 @@
-
+def clean_data(df):
+    df_clean = df[['ru', 'ko']].copy()
+    df_clean.columns = ['source_text', 'target_text']
+    df_clean = df_clean.dropna()
+    df_clean = df_clean[df_clean['source_text'].str.len() > 5]
+    df_clean = df_clean[df_clean['target_text'].str.len() > 5]
+    return df_clean
